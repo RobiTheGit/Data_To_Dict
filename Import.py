f3ef515fabf57d779728346bd3877c9e98d34ffc
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-10-06 16:50:03 -0600

    made this code more efficient

--- Import.py
+++ Import.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 import Data2Dict							#	Import The Data2Dict Scripts 
 Data2Dict.MakeDict(open("data.txt",'r'), "|", "\n", False)		#	Run Data2Dict on the example file, Data2Dict.MakeDict(<datafile>, sep1, sep2, OUTPUTVALUE)
-x = 0									#	Define a counter variable
-while x != len(Data2Dict.Dict):						#	While the counter isn't the same value as the length of the output
+for x in (Data2Dict.Dict):						#	While the counter isn't the same value as the length of the output
     print(f'[{Data2Dict.Dict[x]}]')					#	Print The output
-    x += 1								#	Add one to the counter
