3a031af6606bd00953d844a480c4a292071e276b
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-10-06 16:39:08 -0600

    Update Import.py

--- Import.py
+++ Import.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import Data2Dict							#	Import The Data2Dict Scripts 
-Data2Dict.MakeDict(open("data.txt",'r'), "|", "\n", False)	#	Run Data2Dict on the example file
+Data2Dict.MakeDict(open("data.txt",'r'), "|", "\n", False)	    #	Run Data2Dict on the example file
 x = 0									#	Define a counter variable
 while x != len(Data2Dict.Dict):						#	While the counter isn't the same value as the length of the output
     print(f'[{Data2Dict.Dict[x]}]')					#	Print The output
