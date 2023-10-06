a26faaa48a0c6d517e5c42531fa93cbdf643e5c2
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-10-06 16:23:39 -0600

    did the same commenting I did wit hthe main script

--- Import.py
+++ Import.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-import Data2Dict
-Data2Dict.MakeDict(open("data.txt",'r'), "|", "\n")
-print(Data2Dict.Dict)
+import Data2Dict					#	Import The Data2Dict Scripts
+Data2Dict.MakeDict(open("data.txt",'r'), "|", "\n")	#	Run Data2Dict on the example file
+print(Data2Dict.Dict)					#	Print The output (though the script already prints it)
