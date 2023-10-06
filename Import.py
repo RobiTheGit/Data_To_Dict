7e376fa4c103ead8332e7def785db7a713b07d7c
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-10-06 16:50:47 -0600

    forgot to update a comment

--- Import.py
+++ Import.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import Data2Dict							#	Import The Data2Dict Scripts 
 Data2Dict.MakeDict(open("data.txt",'r'), "|", "\n", False)		#	Run Data2Dict on the example file, Data2Dict.MakeDict(<datafile>, sep1, sep2, OUTPUTVALUE)
-for x in (Data2Dict.Dict):						#	While the counter isn't the same value as the length of the output
+for x in (Data2Dict.Dict):						#	Repeat for every entry in Data2Dict.Dict
     print(f'[{Data2Dict.Dict[x]}]')					#	Print The output
